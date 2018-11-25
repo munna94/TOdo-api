641f5219c7ca51faab6be4511ae703e4d3324166
Munna Kumar <munnakumar@Munnas-MacBook-Air.local>
2018-11-25 12:55:35 +0530

    mongodb addded

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,22 @@
+const Mongoclient=require('mongodb').MongoClient;
+Mongoclient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
+
+if(err){
+  console.log('error while connecting database');
+}
+
+console.log('connected to database successfully');
+
+db.collection('Todo').insertOne({
+  name:'munna1',
+  id:'bpss263'
+},(err,result)=>{
+if(err){
+  return console.log('unable to insert',err);
+}
+console.log(JSON.stringify(result.ops));
+
+});
+db.close();
+
+});
