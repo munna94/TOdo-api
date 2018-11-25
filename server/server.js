30cf02c3ca3f60ed2c3e8ec3d63f27f1be5dcf58
Munna Kumar <munnakumar@Munnas-MacBook-Air.local>
2018-11-25 16:00:12 +0530

    model added

--- server/server.js
+++ server/server.js
@@ -32,6 +32,19 @@ app.post('/todos',(req,res)=>{
 
 });
 
+
+app.get('/todos',(req,res)=>{
+  Todo.find().then((todos)=>{
+     res.send({todos})
+  },(error)=>{
+    res.status(400).send(e);
+
+  });
+
+});
+
+
+
 app.listen(3000,()=>{
   console.log('server started at port 3000');
 })
